c78384dfd477be7734c93e34aefecf5d81300bf5
yolopeanut <brandonqltan@gmail.com>
2024-10-17 23:13:34 +0800

    removed impt react

--- splitbill/src/modules/features/friends/FriendsPage.tsx
+++ splitbill/src/modules/features/friends/FriendsPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const FriendsPage = () => {
 	return <div>FriendsPage</div>;
 };
--- splitbill/src/modules/features/groups/GroupsPage.tsx
+++ splitbill/src/modules/features/groups/GroupsPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const GroupsPage = () => {
 	return <div>GroupsPage</div>;
 };
