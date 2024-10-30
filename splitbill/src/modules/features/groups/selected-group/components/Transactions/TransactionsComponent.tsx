6cc564e0baf3c4f93a5f137b99b4a871a8a1468a
yolopeanut <brandonqltan@gmail.com>
2024-10-30 19:15:16 +0800

    added group by for transaction dates

--- splitbill/src/modules/features/groups/selected-group/components/Transactions/TransactionsComponent.tsx
+++ splitbill/src/modules/features/groups/selected-group/components/Transactions/TransactionsComponent.tsx
@@ -6,6 +6,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import { IAllTransactionsTable } from "../../../../../core/interfaces/all_transactionsTable";
 import { useGroupsContext } from "../../../hooks/useGroupsContext";
 
+interface GroupedTransactions {
+	[key: string]: IAllTransactionsTable[];
+}
+
 const TransactionCard = ({
 	amount,
 	category,
@@ -88,19 +92,44 @@ const Transactions = ({
 		return sortedTransactions;
 	};
 
+	const groupTransactionsByDate = (transactions: IAllTransactionsTable[] | undefined) => {
+		if (!transactions) return {};
+
+		return transactions.reduce((groups: GroupedTransactions, transaction) => {
+			const date = new Date(transaction.created_at).toLocaleDateString();
+			if (!groups[date]) {
+				groups[date] = [];
+			}
+			groups[date].push(transaction);
+			return groups;
+		}, {});
+	};
+
+	const groupedSortedTransactions = groupTransactionsByDate(sortedTransaction(allTransactions));
+
 	return (
 		<div className='flex flex-col justify-start items-center gap-4 w-full h-full'>
-			{sortedTransaction(allTransactions)?.map((transaction) => {
-				const paidBy = groupUsers?.find((user) => user.id === transaction.paid_by);
+			{Object.keys(groupedSortedTransactions).map((groupedDates) => {
 				return (
-					<TransactionCard
-						key={transaction.transaction_id}
-						amount={transaction.total_amount}
-						category={transaction.category as ExpenseCategory}
-						paidBy={paidBy?.name || ""}
-						title={transaction.trans_title || ""}
-						remarks={transaction.remarks || ""}
-					/>
+					<div
+						key={groupedDates}
+						className='flex flex-col justify-start items-start gap-4 w-full'
+					>
+						<span className='text-font-white text-base font-semibold'>{groupedDates}</span>
+						{groupedSortedTransactions[groupedDates].map((transaction) => {
+							const paidBy = groupUsers?.find((user) => user.id === transaction.paid_by);
+							return (
+								<TransactionCard
+									key={transaction.transaction_id}
+									amount={transaction.total_amount}
+									category={transaction.category as ExpenseCategory}
+									paidBy={paidBy?.name || ""}
+									title={transaction.trans_title || ""}
+									remarks={transaction.remarks || ""}
+								/>
+							);
+						})}
+					</div>
 				);
 			})}
 			<FloatingButton handleAddTransactionOnClick={handleAddTransactionOnClick} />
