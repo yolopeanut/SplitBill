0c488cf2d2e9d938185cbfa5ff9aa74276e1b336
yolopeanut <brandonqltan@gmail.com>
2025-01-27 15:51:24 +0800

    Commented out edit profile

--- splitbill/src/modules/features/profile/components/profile-page-body/ProfilePageBody.tsx
+++ splitbill/src/modules/features/profile/components/profile-page-body/ProfilePageBody.tsx
@@ -1,43 +1,42 @@
 import { IoLogOutOutline } from "react-icons/io5";
 import LogoutDrawer from "../LogOutDrawer";
-import { GoPerson } from "react-icons/go";
+// import { GoPerson } from "react-icons/go";
 import { useState } from "react";
 
 const ProfilePageBody = () => {
-	const btnClass =
-		"btn border-none bg-card-gray h-16 rounded-xl flex flex-row items-center gap-6 p-4 justify-start";
-	const iconClass = "text-brand-orange";
-	const textClassGeneric = "text-font-white text-lg font-medium";
+    const btnClass =
+        "btn border-none bg-card-gray h-16 rounded-xl flex flex-row items-center gap-6 p-4 justify-start";
+    // const iconClass = "text-brand-orange";
+    // const textClassGeneric = "text-font-white text-lg font-medium";
 
-	const [isLogoutDrawerOpen, setIsLogoutDrawerOpen] = useState(false);
+    const [isLogoutDrawerOpen, setIsLogoutDrawerOpen] = useState(false);
 
-	return (
-		<div className='w-full flex flex-col gap-4'>
-			<button className={btnClass}>
+    return (
+        <div className='w-full flex flex-col gap-4'>
+            {/* <button className={btnClass}>
 				<GoPerson
 					size={30}
 					className={iconClass}
 				/>
 				<span className={textClassGeneric}>Edit Profile</span>
-			</button>
+			</button> */}
 
-			<button
-				className={btnClass}
-				onClick={() => setIsLogoutDrawerOpen(true)}
-			>
-				<IoLogOutOutline
-					size={30}
-					className='text-font-red-dark'
-				/>
-				<span className='text-font-red-dark text-lg font-medium'>Logout</span>
-			</button>
+            <button
+                className={btnClass}
+                onClick={() => setIsLogoutDrawerOpen(true)}
+            >
+                <IoLogOutOutline size={30} className='text-font-red-dark' />
+                <span className='text-font-red-dark text-lg font-medium'>
+                    Logout
+                </span>
+            </button>
 
-			<LogoutDrawer
-				setIsLogoutDrawerOpen={setIsLogoutDrawerOpen}
-				isLogoutDrawerOpen={isLogoutDrawerOpen}
-			/>
-		</div>
-	);
+            <LogoutDrawer
+                setIsLogoutDrawerOpen={setIsLogoutDrawerOpen}
+                isLogoutDrawerOpen={isLogoutDrawerOpen}
+            />
+        </div>
+    );
 };
 
 export default ProfilePageBody;
